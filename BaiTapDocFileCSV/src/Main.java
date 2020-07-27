8ff39e11303295bc394446023a2d1e071501792f
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-27 13:31:10 +0700

    update

--- BaiTapDocFileCSV/src/Main.java
+++ BaiTapDocFileCSV/src/Main.java
@@ -1,8 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ReadCSVFile test=new ReadCSVFile("test");
-        System.out.println(test.splitNumber());
+
+    static Map<String, String> map = new HashMap<String, String>();
+
+    public static void main(String[] args) {
+
+        String csvFile = "test.CSV";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+                for (int i = 0; i < country.length; i++) {
+                    map.put(country[4], country[5]);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String code = entry.getKey();
+            String name = entry.getValue();
+            System.out.println("Country [code= " + code + " , name=" + name + "]");
+        }
     }
+
 }
--- BaiTapDocFileCSV/src/ReadCSVFile.java
+++ BaiTapDocFileCSV/src/ReadCSVFile.java
@@ -1,47 +1,47 @@
-import java.io.*;
-
-public class ReadCSVFile {
-    File source;
-    Reader reader;
-    BufferedReader bufferedReader;
-    Writer writer;
-    String cvsSplitBy = ",";
-
-
-    public ReadCSVFile (String fileName){
-        source=new File(fileName+".CSV");
-        if(!source.exists()){
-            System.err.println("File Khong Ton Tai");
-        }
-    }
-
-    public String readRawContent() throws IOException {
-        String s;
-        if (source.isFile()) {
-            bufferedReader = new BufferedReader(new FileReader(source));
-            StringBuffer buffer = new StringBuffer("");
-            String row;
-            while ((row = bufferedReader.readLine()) != null) {
-                buffer.append(bufferedReader.readLine());
-            }
-            s = buffer.toString();
-            bufferedReader.close();
-            return s;
-        }
-        else return "khong tim thay file";
-
-    }
-
-    public String[] toArray() throws IOException {
-        String chuoiCanTach= readRawContent();
-        StringBuffer append=new StringBuffer("");
-
-        for(int i=0; i<chuoiCanTach.length();i++){
-            while(Character.toString(chuoiCanTach.charAt(i))!=cvsSplitBy){
-                append.append(Character.toString(chuoiCanTach.charAt(i));
-            }
-        }
-
-    }
-
-}
+//import java.io.*;
+//
+//public class ReadCSVFile {
+//    File source;
+//    Reader reader;
+//    BufferedReader bufferedReader;
+//    Writer writer;
+//    String cvsSplitBy = ",";
+//
+//
+//    public ReadCSVFile (String fileName){
+//        source=new File(fileName+".CSV");
+//        if(!source.exists()){
+//            System.err.println("File Khong Ton Tai");
+//        }
+//    }
+//
+//    public String readRawContent() throws IOException {
+//        String s;
+//        if (source.isFile()) {
+//            bufferedReader = new BufferedReader(new FileReader(source));
+//            StringBuffer buffer = new StringBuffer("");
+//            String row;
+//            while ((row = bufferedReader.readLine()) != null) {
+//                buffer.append(bufferedReader.readLine());
+//            }
+//            s = buffer.toString();
+//            bufferedReader.close();
+//            return s;
+//        }
+//        else return "khong tim thay file";
+//
+//    }
+//
+//    public String[] toArray() throws IOException {
+//        String chuoiCanTach= readRawContent();
+//        StringBuffer append=new StringBuffer("");
+//
+//        for(int i=0; i<chuoiCanTach.length();i++){
+//            while(Character.toString(chuoiCanTach.charAt(i))!=cvsSplitBy){
+//                append.append(Character.toString(chuoiCanTach.charAt(i)i);
+//            }
+//        }
+//
+//    }
+//
+//}
