e9b3db898eda55b041f5113fab27bec44b141f16
Phamtung1802 <phamtung.0218@gmail.com>
2020-07-27 10:42:07 +0700

    update thuc hanh

--- BaiTapSaoChep/src/DemoFile.java
+++ BaiTapSaoChep/src/DemoFile.java
@@ -7,42 +7,41 @@ public class DemoFile {
     Writer writer;
 
     public DemoFile(String input) throws FileNotFoundException {
-        sourceFile= new File(input+".txt");
-
-
+        sourceFile = new File(input + ".txt");
     }
 
-    public File getSourceFile(){
+    public File getSourceFile() {
         return sourceFile;
     }
 
     public String getSourceFileContent() throws IOException {
-        this.reader=new FileReader(this.sourceFile);
-        StringBuffer fileContent=new StringBuffer("");
+        this.reader = new FileReader(this.sourceFile);
+        StringBuffer fileContent = new StringBuffer("");
         int i = -1;
         while ((i = reader.read()) != -1) {
-           fileContent.append((char)i);
+            fileContent.append((char) i);
         }
         reader.close();
         return fileContent.toString();
     }
+
     public String getOutputFileContent() throws IOException {
-        this.reader=new FileReader(this.outputFile);
-        StringBuffer fileContent=new StringBuffer("");
+        this.reader = new FileReader(this.outputFile);
+        StringBuffer fileContent = new StringBuffer("");
         int i = -1;
         while ((i = reader.read()) != -1) {
-            fileContent.append((char)i);
+            fileContent.append((char) i);
         }
         reader.close();
         return fileContent.toString();
     }
+
     public void copyFileContent(String tenfile) throws IOException {
-        outputFile=new File(tenfile+".txt");
-       if(!this.outputFile.exists()&&!this.outputFile.isDirectory()) {
-           this.writer = new FileWriter(this.outputFile);
-           writer.write(this.getSourceFileContent());
-           writer.close();
-       }
-       else System.out.println("File da ton tai");
+        outputFile = new File(tenfile + ".txt");
+        if (!this.outputFile.exists() && !this.outputFile.isDirectory()) {
+            this.writer = new FileWriter(this.outputFile);
+            writer.write(this.getSourceFileContent());
+            writer.close();
+        } else System.out.println("File da ton tai");
     }
 }
